e76efe6d7e4ffc236179d4c716973313dbfa68cd
Francisco <fcocacerescolihuil@gmail.com>
2025-07-09 13:29:47 -0400

    add send system metrics to api

--- client.py
+++ client.py
@@ -1,6 +1,14 @@
 import datetime
+import os
+import time
 
 import psutil
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = os.getenv('API_URL')
 
 
 def get_system_metrics():
@@ -12,3 +20,13 @@ def get_system_metrics():
         'cpu_usage': cpu_usage,
         'memory_usage': memory_info.percent,
     }
+
+
+def send_system_metrics(metrics):
+    requests.post(API_URL + '/system-metrics', json=metrics)
+
+
+while True:
+    current_metrics = get_system_metrics()
+    send_system_metrics(current_metrics)
+    time.sleep(1)
