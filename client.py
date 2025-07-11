f54979e52a901bb99ccb6ce2f0e3a438a203cb64
Francisco <fcocacerescolihuil@gmail.com>
2025-07-10 23:17:05 -0400

    add send logs to api

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_URL = os.getenv('API_URL')
+LOGS_FILE_PATH = os.getenv('LOGS_FILE_PATH')
 
 
 def get_system_metrics():
@@ -26,7 +27,53 @@ def send_system_metrics(metrics):
     requests.post(API_URL + '/system-metrics', json=metrics)
 
 
+def get_logs(last_position=0):
+    logs = []
+    current_position = last_position
+    with open(LOGS_FILE_PATH, 'r') as log_file:
+        log_file.seek(last_position)
+        lines = log_file.readlines()
+        for line in lines:
+            parsed_log = parse_log_line(line)
+            logs.append(parsed_log)
+        current_position = log_file.tell()
+    return logs, current_position
+
+
+def parse_log_line(line):
+    parts = line.strip().split(' ', 3)
+    timestamp = parts[0]
+    hostname = parts[1]
+    process_and_pid = parts[2]
+    if '[' in process_and_pid and ']' in process_and_pid:
+        process = process_and_pid.split('[')[0]
+        pid = process_and_pid.split('[')[1].split(']')[0]
+    else:
+        process = process_and_pid.rstrip(':')
+        pid = ''
+    message = parts[3] if len(parts) > 3 else ''
+    return {
+        'timestamp': timestamp,
+        'hostname': hostname,
+        'process': process,
+        'pid': pid,
+        'message': message
+    }
+
+
+def send_logs(logs):
+    if logs:
+        requests.post(API_URL + '/logs', json=logs)
+
+
+last_position = 0
+
 while True:
     current_metrics = get_system_metrics()
     send_system_metrics(current_metrics)
+
+    logs, last_position = get_logs(last_position)
+    if logs:
+        send_logs(logs)
+
     time.sleep(1)
